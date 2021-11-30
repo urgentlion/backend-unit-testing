f7479cef3626a3ebe20c44803de1dfc035e28594
urgentlion <urgentlioninfo@gmail.com>
2021-11-30 14:42:26 -0800

    ...

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json('hello world');
+    res.json("hello world");
 });
 
 app.listen(3000, () => {
-    console.log('server is running');
-});
+    console.log('server is now running');
+})
--- tests/unit/server.controller.test.js
+++
@@ -1,7 +0,0 @@
-const ServerController = require('../../controllers/server.controller');
-
-describe('ServerController.createTodo', () => {
-    it('should have a create todo function', () => {
-        expect(typeof ServerController.createTodo).toBe('function'); 
-    });
-});

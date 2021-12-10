a8311c1895a77206f46b9b8b1535659647afe4dd
urgentlion <urgentlioninfo@gmail.com>
2021-12-10 14:52:51 -0800

    ...

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,5 +1,6 @@
 const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../model/todo.model");
+const httpMocks = require("node-mocks-http");
 
 //If you do not have the jest.fn overriding the original fn, then you can't see the fn being called. This must be a mock or a spy
 TodoModel.create = jest.fn();
@@ -10,7 +11,11 @@ describe("TodoController.createTodo", () => {
     });
     //test to make sure these methods are being called
     it("should call TodoModel.create", () => {
-        TodoController.createTodo();
+        let req, res, next;
+        req = httpMocks.createRequest();
+        res = httpMocks.createResponse();
+        next = null;
+        TodoController.createTodo(req, res, next);
         expect(TodoModel.create).toBeCalled();
     })
 });
